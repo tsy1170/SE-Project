39cb9e2159428fb081dc41f937fc226c44c2ff83
tsy1170 <tsy1170@gmail.com>
2025-06-21 02:17:23 +0800

    Update Login.py
    style

--- Login.py
+++ Login.py
@@ -1,7 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
+
+from pandas import wide_to_long
+
 import user_page
 import admin_page
 
@@ -50,18 +53,25 @@ def login(root, entry_ID, entry_password):
 
 def show_login():
     root = tk.Tk()
+    root.configure(bg="White")
     root.title("Login")
-    root.geometry("300x200")
+    root.geometry("320x220")
 
-    tk.Label(root, text="UserID:").pack(pady=5)
-    entry_ID = tk.Entry(root)
+    style = ttk.Style()
+    style.theme_use("clam")
+    style.configure("Bold.TButton", font=("Segoe UI", 10, "bold"), width=10, border=15)
+    style.configure("Custom.TLabel", background="White", foreground="#333333", font=("Segoe UI", 10, "bold"), padding=5)
+    style.configure("Custom.TEntry", foreground="black", fieldbackground="lightyellow", font=("Segoe UI", 10))
+
+    ttk.Label(root, text="Enter ID:", style="Custom.TLabel").pack()
+    entry_ID = ttk.Entry(root, style="Custom.TEntry", width=30)
     entry_ID.pack(pady=5)
 
-    tk.Label(root, text="Password:").pack(pady=5)
-    entry_password = tk.Entry(root, show="*")
+    ttk.Label(root, text="Enter password:", style="Custom.TLabel").pack()
+    entry_password = ttk.Entry(root, show="*", style="Custom.TEntry", width=30)
     entry_password.pack(pady=5)
 
-    tk.Button(root, text="Login", command=lambda: login(root, entry_ID, entry_password)).pack(pady=15)
+    ttk.Button(root, text="Login", style="Bold.TButton", command=lambda: login(root, entry_ID, entry_password)).pack(pady=15)
 
     root.mainloop()
 
